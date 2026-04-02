e47c34f9f6b0a57d8f845ed47628169b4ff76796
Shaheem Niyaz <shaheembn@gmail.com>
2026-04-02 12:37:07 +0530

    feat: server-side form validation

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,18 +1,29 @@
+import { body, matchedData, validationResult } from "express-validator";
 import { createPost } from "../db/queries.js";
 
 export function getNewPage(_req, res) {
   res.render("new");
 }
 
-export async function postNewNote(req, res) {
-  const { author, note, color } = req.body;
+export const postNewNote = [
+  [
+    body("author").trim().notEmpty().withMessage("Author should not be empty"),
+    body("note").trim().notEmpty().withMessage("Note should not be empty"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("new", { errors: errors.array() });
+    }
 
-  await createPost({
-    author,
-    note,
-    color: color ?? "yellow",
-    rotation: Math.random() * 10 - 5,
-  });
+    const { author, note, color } = matchedData(req);
+    await createPost({
+      author,
+      note,
+      color: color ?? "yellow",
+      rotation: Math.random() * 10 - 5,
+    });
 
-  res.redirect("/");
-}
+    res.redirect("/");
+  },
+];
